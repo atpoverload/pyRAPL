290509b8ab5e910766aec4fdd07fd4f31306500f
atpoverload <atpoverload@gmail.com>
2026-09-07 13:14:25 -0600

    hmmm

--- pyRAPL/device_api.py
+++ pyRAPL/device_api.py
@@ -78,6 +78,7 @@ class DeviceAPI:
 
         self._sys_files = self._open_rapl_files()
         self.wraparound = self._get_wraparound_values()
+        print(self.wraparound)
 
     def _open_rapl_files(self):
         raise NotImplementedError()
