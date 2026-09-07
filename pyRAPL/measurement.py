40b95648b714000d1237b50e6533abd1763f476a
atpoverload <atpoverload@gmail.com>
2026-09-07 13:07:25 -0600

    little logging to save me a headache

--- pyRAPL/measurement.py
+++ pyRAPL/measurement.py
@@ -77,7 +77,14 @@ class Measurement:
         energy_end = self._sensor.energy()
 
         delta = energy_end - self._energy_begin
-        delta = [(e + w) if e < 0 else e for e, w in zip(delta, self._sensor.wraparound())]
+        def wraparound_adjustment(e, w):
+            if e < 0:
+                print("adjusting negative value %d", e)
+                return e + w
+            else:
+                return e
+
+        delta = list(map(wraparound_adjustment, zip(delta, self._sensor.wraparound())))
         duration = ts_end - self._ts_begin
         pkg = delta[0::2]  # get odd numbers
         pkg = pkg if empty_energy_result(pkg) else None  # set result to None if its contains only -1
